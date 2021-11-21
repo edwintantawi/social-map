17071bbc04846e08daeab708814b96c9651ddcd4
edwintantawi <edwintantawi@gmail.com>
2021-11-21 23:02:08 +0700

    🚀 create configuration file

---
+++ src/config/index.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const getEnv = (envKey) => {
+  const env = process.env[envKey];
+  if (env === undefined) {
+    throw new Error(`[ ENV ] Fail to load Env: "${envKey}"`);
+  }
+  return env;
+};
+
+export const serverOptions = {
+  port: process.env.PORT || 5000,
+};
+
+export const mysqlDatabaseOptions = {
+  host: getEnv('MYSQL_HOST'),
+  user: getEnv('MYSQL_USER'),
+  password: getEnv('MYSQL_PASSWORD'),
+  database: getEnv('MYSQL_DATABASE'),
+};
