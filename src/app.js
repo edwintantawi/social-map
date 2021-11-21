f7a39e9539d0d6a287e7eea0f4809162d4557ec2
edwintantawi <edwintantawi@gmail.com>
2021-11-21 17:33:15 +0700

    🚀 add threads

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import threadRouters from './routers/threads.router.js';
 
 const PORT = 5000;
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (_, res) => res.json({ message: 'ok' }));
+
+app.use('/threads', threadRouters);
 
 app.listen(PORT, () => {
   console.log(`Server start at http://localhost:${PORT}`);
