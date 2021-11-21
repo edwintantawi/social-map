2458f7aac501233150fb8e67ab997bf2f51b5c4b
edwintantawi <edwintantawi@gmail.com>
2021-11-22 00:41:13 +0700

    🚀 use views

--- src/app.js
+++ src/app.js
@@ -1,18 +1,27 @@
 import express from 'express';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 // config
 import { serverOptions } from './config/index.js';
 
 // routers
-import threadRouters from './routers/threads.router.js';
+import threadsRouter from './routers/threads.router.js';
+import viewsRouter from './routers/views.router.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
-app.get('/', (_, res) => res.json({ message: 'ok' }));
-
-app.use('/threads', threadRouters);
+app.use('/', viewsRouter);
+app.use('/threads', threadsRouter);
 
 app.listen(serverOptions.port, () => {
   console.log(`Server start at http://localhost:${serverOptions.port}`);
